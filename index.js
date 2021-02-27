a00d5acc510805ce3f6f008f2d9a5c9c0674d1e5
vinhnt0910 <vinhnt0910@gmail.com>
2021-02-27 22:14:10 +0700

    test

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 function shopFunction() {
     // document.getElementById("social").classList.remove('fb-customerchat');
     // document.getElementById("shop").classList.add('fb-customerchat'); 
-    document.getElementById("social").className = "fb-customerchat-social";
-    document.getElementById("shop").className = "fb-customerchat";
+    document.getElementById("shop").setAttribute("page_id", "102167945262445");
+    // document.getElementById("shop").className = "fb-customerchat";
     alert("Shop");
 }
 
 function socialFunction() {
     // document.getElementById("shop").classList.remove('fb-customerchat');
     // document.getElementById("social").classList.add('fb-customerchat');
-    document.getElementById("shop").className = "fb-customerchat-shop";
-    document.getElementById("social").className = "fb-customerchat";
+    // document.getElementById("shop").className = "fb-customerchat-shop";
+    // document.getElementById("social").className = "fb-customerchat";
+    document.getElementById("shop").setAttribute("page_id", "107507041389912");
     alert("Social");
 }
